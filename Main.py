7fcc3c41ebca645dc2da13e5d1efddb31dbdaa32
Katakuri <129244078+albieking1@users.noreply.github.com>
2023-11-08 13:51:42 +1100

    Create Main.py
    The code for this game

---
+++ Main.py
@@ -0,0 +1,13 @@
+import random
+n = random.randrange(1,10)
+guessednumber = int(input("Guess any number from 1 to 10: "))
+while!= guessednumber:
+  if guessednumber < n:
+    print("Too low!")
+    guessednumber = int(input("Guess again: "))
+  elif guessednumber > n:
+    print("Too high!")
+    guessednumber = int(input("Guess again: "))
+  else:
+    break
+    print(f'Correct, the answer was {n}!')
